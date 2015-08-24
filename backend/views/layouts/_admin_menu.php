c42e951060cbb039cddfa4cc9ddc4861cebee16a
Loveorigami <loveorigami@mail.ru>
2015-08-24 17:15:01 +0300

    fix rbac admin menu

--- backend/views/layouts/_admin_menu.php
+++ backend/views/layouts/_admin_menu.php
@@ -1,8 +1,9 @@
 <ul class="nav nav-tabs">
-    <li><a href="/admin/#/assignment"><?=\Yii::t('backend','Assignment')?></a></li>
-    <li><a href="/admin/#/role"><?=\Yii::t('backend','Role')?></a></li>
-    <li><a href="/admin/#/permission"><?=\Yii::t('backend','Permission')?></a></li>
-    <li><a href="/admin/#/rule"><?=\Yii::t('backend','Rule')?></a></li>
-    <li><a href="/admin/#/route"><?=\Yii::t('backend','Route')?></a></li>
-    <li><a href="/admin/#/menu"><?=\Yii::t('backend','Menu')?></a></li>
+    <li><a href="#/assignment"><?=\Yii::t('backend','Assignment')?></a></li>
+    <li><a href="#/role"><?=\Yii::t('backend','Role')?></a></li>
+    <li><a href="#/permission"><?=\Yii::t('backend','Permission')?></a></li>
+    <li><a href="#/rule"><?=\Yii::t('backend','Rule')?></a></li>
+    <li><a href="#/route"><?=\Yii::t('backend','Route')?></a></li>
+    <li><a href="#/menu"><?=\Yii::t('backend','Menu')?></a></li>
 </ul>
+<div class="clear_small"></div>
