431df9579f882081cf748163e0f8ab39bed6214e
Loveorigami <loveorigami@mail.ru>
2015-08-25 13:29:39 +0300

    admin 3 active menu

--- backend/views/layouts/_admin_menu.php
+++ backend/views/layouts/_admin_menu.php
@@ -1,9 +1,6 @@
-<ul class="nav nav-tabs">
-    <li><a href="#/assignment"><?=\Yii::t('backend','Assignment')?></a></li>
-    <li><a href="#/role"><?=\Yii::t('backend','Role')?></a></li>
-    <li><a href="#/permission"><?=\Yii::t('backend','Permission')?></a></li>
-    <li><a href="#/rule"><?=\Yii::t('backend','Rule')?></a></li>
-    <li><a href="#/route"><?=\Yii::t('backend','Route')?></a></li>
-    <li><a href="#/menu"><?=\Yii::t('backend','Menu')?></a></li>
+<ul class="nav nav-tabs" ng-controller="HeaderMenuCtrl">
+    <li ng-repeat="menu in headerMenu" ng-class="{active:location.path().indexOf(menu.id)===0}">
+        <a ng-href="{{menu.url}}" ng-bind="menu.label"></a>
+    </li>
 </ul>
 <div class="clear_small"></div>
