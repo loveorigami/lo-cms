6474990cab549f82e8fbbf1f2e321911328f514e
Loveorigami <loveorigami@mail.ru>
2015-09-06 15:20:41 +0300

    environments

---
+++ environments/backend/config/params-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'enabledModules' => [
+        [
+            'path' =>'@vendor/loveorigami/lo-module-{module}/modules/admin/config/main.php',
+            'modules'=>['playground', 'core', 'page']
+        ]
+    ],
+];
