e60761090aabe3550e1f17bdaae7a20f8a77c938
Loveorigami <loveorigami@mail.ru>
2016-04-10 12:38:44 +0300

    elfinder config move to module

--- backend/config/main.global.php
+++ backend/config/main.global.php
@@ -11,42 +11,6 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log', 'plugins', 'admin'],
-    'controllerMap' => [
-        'elfinder' => [
-            'class' => '\mihaildev\elfinder\PathController',
-            'access' => ['admin'], //глобальный доступ к фаил менеджеру @ - для авторизорованных , ? - для гостей , чтоб открыть всем ['@', '?']
-            'disabledCommands' => ['netmount'], //отключение ненужных команд https://github.com/Studio-42/elFinder/wiki/Client-configuration-options#commands
-            'root' => [
-                'baseUrl' => '', // /uploads
-                'basePath' => '@storage', // site.lo/uploads
-                'access' => ['read' => '*', 'write' => 'root'],
-                'name' => ['category' => 'backend', 'message' => 'Category'], // Yii::t($category, $message)
-                'driver' => 'LocalFileSystem',
-                'options' => [
-                    'tmbSize' => '48',
-                    'acceptedName' => '/^[0-9a-z_\-.]+$/i', // i любой регистр только англ
-                    'imgLib' => 'gd'
-                ]
-            ]
-        ],
-        'elfinder-editor' => [
-            'class' => '\mihaildev\elfinder\PathController',
-            'access' => ['admin'], //глобальный доступ к фаил менеджеру @ - для авторизорованных , ? - для гостей , чтоб открыть всем ['@', '?']
-            'disabledCommands' => ['netmount'], //отключение ненужных команд https://github.com/Studio-42/elFinder/wiki/Client-configuration-options#commands
-            'root' => [
-                'baseUrl' => '@editorUrl', // /uploads
-                'basePath' => '@storage', // site.lo/uploads
-                'access' => ['read' => '*', 'write' => 'root'],
-                'name' => ['category' => 'backend', 'message' => 'Category'], // Yii::t($category, $message)
-                'driver' => 'LocalFileSystem',
-                'options' => [
-                    'tmbSize' => '48',
-                    'acceptedName' => '/^[0-9a-z_\-.]+$/i', // i любой регистр только англ
-                    'imgLib' => 'gd'
-                ]
-            ]
-        ]
-    ],
     'modules' => [
         'plugins' => [
             'class' => 'lo\plugins\Module',
