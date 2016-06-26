f5931c6af4dd2816b8baa70415fcc9d926852531
Loveorigami <loveorigami@mail.ru>
2016-06-26 14:37:55 +0300

    sxd config

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,9 +4,12 @@ Yii::setAlias('common', dirname(__DIR__));
 Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
-Yii::setAlias('storage', dirname(dirname(__DIR__)) . '/www/'.getenv('STORAGE_DIR'));
-Yii::setAlias('storageb', dirname(dirname(__DIR__)) . '/www/_admin/'.getenv('STORAGE_DIR'));
+Yii::setAlias('storage', dirname(dirname(__DIR__)) . '/web'.getenv('STORAGE_DIR'));
+Yii::setAlias('storageb', dirname(dirname(__DIR__)) . '/web/'.getenv('ADMIN_PATH').'/'.getenv('STORAGE_DIR'));
 Yii::setAlias('vendor', dirname(dirname(__DIR__)) . '/vendor');
+
+Yii::setAlias('sxd', dirname(dirname(__DIR__)) . '/web/'.getenv('ADMIN_PATH').'/assets/sxd');
+Yii::setAlias('@sxdUrl', getenv('BACKEND_URL').'/assets/sxd');
 
 // Url Aliases
 Yii::setAlias('@frontendUrl', getenv('FRONTEND_URL'));
